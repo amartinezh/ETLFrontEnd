0f681e58b14ac347bc2cae04b78f755bbee243c0
Carolina Cardona Garcia <ccardona@veritran.com>
2021-07-03 14:14:35 -0500

    Agrego nuevo servicio

---
+++ src/app/services/validator/validator.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { BaseServiceService } from '../base-services/base-service.service';
+import PeopleModel from '../../models/filter.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidatorService {
+
+  constructor() { }
+}
