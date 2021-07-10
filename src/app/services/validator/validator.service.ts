517b0f483e0366022b52dc4c96e3663d2b3bb3ab
Carolina Cardona Garcia <ccardona@veritran.com>
2021-07-09 21:51:54 -0500

    Se completa el servicio validador

--- src/app/services/validator/validator.service.ts
+++ src/app/services/validator/validator.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
 import { BaseServiceService } from '../base-services/base-service.service';
-import PeopleModel from '../../models/filter.model'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import FilterModel from '../../models/filter.model'
 
 @Injectable({
   providedIn: 'root'
 })
-export class ValidatorService {
+export class ValidatorService extends BaseServiceService {
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    super(http);
+   }
+
+   validatorBack() {
+		const endpoint = '/validator/filter';
+		try {
+			return this.consumeAPI(endpoint);
+		} catch (e) {
+			console.log(`An error has occurred validating the user: ${e}`);
+		}
+	}
+
+
 }
--- src/app/views/base/cards.component.ts
+++ src/app/views/base/cards.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { SubirExcelService } from '../../services/grupo-uno/subirExcelService';
+import { ValidatorService } from '../../services/validator/validator.service';
+
 
 
 @Component({
@@ -10,15 +12,19 @@ export class CardsComponent {
   fileToUpload: File | null = null;
   excelBase;
   mostrarBtnEnviar:boolean = false;
-  constructor(public fileUploadService: SubirExcelService) {
+  mostrarBtnValidar:boolean = false;
+  constructor(public fileUploadService: SubirExcelService,
+    public validatorService : ValidatorService) {
 
   }
+
+
 
   handleFileInput(files: FileList) {
     console.log("Entra");
     this.fileToUpload = files.item(0);
     if (this.fileToUpload.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
-      this.mostrarBtnEnviar = true;
+      this.mostrarBtnEnviar = true;      
       const reader = new FileReader();
       reader.readAsDataURL(this.fileToUpload);
       reader.onload = () => {
@@ -26,6 +32,7 @@ export class CardsComponent {
       };
     } else {
       this.mostrarBtnEnviar = false;
+      this.mostrarBtnValidar = false;
       alert("Debe seleccionar un archivo xlsx");
     }
   }
@@ -33,6 +40,12 @@ export class CardsComponent {
   async llamarServicioExcel() {
     var salida = this.excelBase.split(',');
     let value = await this.fileUploadService.enviarExcelBack(salida[1]);
+    this.mostrarBtnEnviar = false;
+    this.mostrarBtnValidar = true;
+  }
+
+  async llamarServicioValidar() {
+    let value = await this.validatorService.validatorBack();
   }
 
 }
